95547318f77542a0f874fc5de5bc38364cfad689
Kai <kaisoapbox@gmail.com>
2024-02-13 13:05:44 -0800

    feat: add google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,19 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://kaisoapbox.github.io",
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    react(),
+    partytown({ config: { forward: ["dataLayer.push"] } }),
+  ],
   redirects: {
     "/faa_biographical_assessment": "/projects/faa_biographical_assessment",
   },
