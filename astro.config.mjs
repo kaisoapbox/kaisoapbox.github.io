fe3b0b61abb1fb200c66fd010ed92f4c5caf97fe
Kai <kaisoapbox@gmail.com>
2024-02-13 13:42:33 -0800

    feat: who needs partytown anyway

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,13 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import partytown from "@astrojs/partytown";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://kaisoapbox.github.io",
-  integrations: [
-    mdx(),
-    sitemap(),
-    react(),
-    partytown({ config: { forward: ["dataLayer.push"] } }),
-  ],
+  integrations: [mdx(), sitemap(), react()],
   redirects: {
     "/faa_biographical_assessment": "/projects/faa_biographical_assessment",
   },
