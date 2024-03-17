ccc20bf085b3e4d503e1a9d2cf142c09d4751fc9
Kai <kaisoapbox@gmail.com>
2024-03-17 14:32:34 -0700

    feat: add buttondown subscribe

---
+++ src/components/ButtondownSubscribe.jsx
@@ -0,0 +1,29 @@
+export default function Subscribe(props) {
+  return (
+    <form
+      action="https://buttondown.email/api/emails/embed-subscribe/kaisoapbox"
+      method="post"
+      target="popupwindow"
+      onSubmit="window.open('https://buttondown.email/kaisoapbox', 'popupwindow')"
+      className="embeddable-buttondown-form"
+    >
+      <label htmlFor="bd-email">
+        Subscribe to my highly infrequent newsletter!
+      </label>
+      <div>
+        <input
+          type="email"
+          name="email"
+          id="bd-email"
+          placeholder="you@example.com"
+        />
+        <input type="submit" value="Subscribe" id="bd-submit" />
+      </div>
+      <p class="buttondown-plug">
+        <a href="https://buttondown.email/refer/kaisoapbox" target="_blank">
+          Powered by Buttondown.
+        </a>
+      </p>
+    </form>
+  );
+}
